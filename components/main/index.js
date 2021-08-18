0144f6dff6f942dcbd75e595419b6f1cb4f31057
Armand SALLE <armand.salle@gmail.com>
2021-08-18 13:00:22 +0200

    Update index.js

--- components/main/index.js
+++ components/main/index.js
@@ -3,10 +3,10 @@ const Main = () => {
     <main className="main">
       <h1 className="h1">
         <span className="first-line fade-1">Ag teacht go luath</span>
-        <span className="second-line fade-2">Launching Summer 2021</span>
+        <span className="second-line fade-2">Launching Winter 2021</span>
       </h1>
       <p className="content fade-3">
-        Saol is a multidisciplinary studio. We believe in a more sustainable future, by design.
+        Saol is a multidisciplinary studio based on the west coast of Ireland. We believe in a more sustainable future by design.
       </p>
     </main>
   )
